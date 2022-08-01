645563d6aea4f66c6b8496c6a4a216932db41221
farina00 <>
2022-08-01 21:27:26 +0100

    go

--- builds/build.js
+++ builds/build.js
@@ -6,6 +6,7 @@
 
 
 const fse = require('fs-extra');
+const path = require('path');
 const { minify } = require("terser");
 const  htmlMinifier = require('html-minifier-terser');
 const CleanCSS = require('clean-css');
@@ -20,7 +21,24 @@ async function compress(filename) {
 }
 
 
+function *walkSync(dir) {
+  const files = fse.readdirSync(dir, { withFileTypes: true });
+  for (const file of files) {
+    if (file.isDirectory()) {
+      yield* walkSync(path.join(dir, file.name));
+    } else {
+      yield path.join(dir, file.name);
+    }
+  }
+}
+
+
+
+
+
 (async function build() {
+
+
 
   fse.emptyDirSync('./docs'); 
   console.log('removed any file in /docs'); 
@@ -29,16 +47,33 @@ async function compress(filename) {
   fse.copySync('./src', './docs'); 
   console.log('copied everything from /src to /docs'); 
 
-  console.log('compressing the js files with terser');
+  console.log('compressing all the resources in docs'); 
 
-  compress('./docs/libs/createBillboard.js');
-  compress('./docs/libs/createMonitoringTable.js');
-  compress('./docs/libs/createParseCurl.js');
-  compress('./docs/libs/createRestEngine.js');
-  compress('./docs/libs/createSTPEngine.js'); 
-  compress('./docs/console.html');
-  compress('./docs/51.css');
-  
+  for (const file of walkSync('./docs')) {
+    console.log('compressing: '+file);
+    compress(file);
+  }
+  /*
+  for (const file of array) {
+    console.log('compressing '+file); 
+    await compress(file); 
+  }
+  */
+ /*
+  await compress('./docs/libs/createBillboard.js');
+  await compress('./docs/libs/createMonitoringTable.js');
+  await compress('./docs/libs/createParseCurl.js');
+  await compress('./docs/libs/createRestEngine.js');
+  await compress('./docs/libs/createSTPEngine.js'); 
+  await compress('./docs/console.html');
+  await compress('./docs/dashboard.html');
+  await compress('./docs/docs.html');
+  await compress('./docs/docs.banking.operations.html');
+  await compress('./docs/docs.get.started.html');
+  await compress('./docs/errors.html');
+  await compress('./docs/examples.html');
+  // await compress('./docs/51.css');
+  */
   
   console.log('build completed !!!');
 
